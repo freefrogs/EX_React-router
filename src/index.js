d445001ee61a80d6ccd7e13241da81f27e7d48a1
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-03-29 16:30:06 +0200

    Add reducer, store & provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store/index';
 //import App from './App';
 
-
-/*ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();*/
-
-ReactDOM.render(
-	<h1>New project: country-app</h1>,
+render(
+	<Provider store={store}>
+		<h1>New project: country-app</h1>
+	</Provider>,
 	document.getElementById('root')
 );
