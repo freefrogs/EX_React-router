d6f415f708a81125e52400a0cbaf80b17c41b9e1
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-03-29 13:06:59 +0200

    Add basic files structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+
+
+/*ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();*/
+
+ReactDOM.render(
+	<h1>New project: country-app</h1>,
+	document.getElementById('root')
+);
